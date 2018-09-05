8945a5540e5592b69687e614eefb65a532ed15be
Kevin Schmitz <kfschmitz@gmail.com>
2018-09-05 16:44:03 -0700

    changed config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/node_capstone'
+exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/testnode_capstone';
 exports.PORT = process.env.PORT || 8080;
 
